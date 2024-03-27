47caaebc4f14789411c7ad4b11780b32acdb2fe2
Long Nguyen <longnd998@gmail.com>
2024-03-27 17:01:11 +0700

    fake book

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ from pymongo.mongo_client import MongoClient
 import os
 
 # uri = "mongodb://root:root@localhost/"
-uri = os.environ.get('MONGO_URI')
+uri = "mongodb://root:root@localhost:27017/"
+# uri = os.environ.get('MONGO_URI')
 client = MongoClient(uri)
 
 # client = MongoClient('db',
@@ -27,7 +28,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    messages = db_collection.find().sort('_id', -1).limit(10)
+    messages = db_collection.find().sort('_id', 1)
     return render_template('index.html', messages=messages)
 
 @app.route('/send', methods=['POST'])
